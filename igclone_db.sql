2fd2f49904e6e69916483bbf4223a33ff644c92a
Maria Clara Ferraz <167479746+claramferraz@users.noreply.github.com>
2026-04-20 20:37:32 -0400

    Criação unfollows table

--- igclone_db.sql
+++ igclone_db.sql
@@ -55,3 +55,12 @@ tag_id INT NOT NULL,
 FOREIGN KEY (photo_id) REFERENCES photos(id),
 FOREIGN KEY (tag_id) REFERENCES tags(id)
 );
+
+CREATE TABLE unfollows(
+follower_id INT NOT NULL,
+followee_id INT NOT NULL,
+created_at TIMESTAMP DEFAULT NOW(),
+FOREIGN KEY (follower_id) REFERENCES users(id),
+FOREIGN KEY (followee_id) REFERENCES users(id),
+PRIMARY KEY(follower_id, followee_id)
+);
